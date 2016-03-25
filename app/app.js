4912fc552e8f5fb715bdceea2514338ca5f2933f
Michael Pam Bennette <KingofTenn@Michaels-MacBook-Air.local>
2016-03-25 15:15:26 -0500

    working on login and module framework

--- app/app.js
+++ app/app.js
@@ -1,22 +1,52 @@
-var myTC = angular.module("myTC", []);
+"use strict";
 
-myTC.config(['$routeProvider', function ($routeProvider) {
+var tC = angular.module("myTCApp", ["ngRoute", "firebase"]);
+  // .constant("firebaseURL", "https://resplendent-inferno-6925.firebaseio.com/");
 
-  $routeProvider
-  .when('/', {
-    templateUrl: 'partials/main.html',
-    controller: "mainCtrl"
-  })
-  .when('/login', {
-    templateUrl: 'partials/login.html',
-    controller: "loginCtrl"
-  })
-  .when('/selector', {
-    templateUrl: 'partials/login.html',
-    controller: ""
-  })
-  .otherwise({
-    redirectTo: '/'
-  });
+/*
+  Define a promise for any view that needs an authenticated user
+  before it will resolve (see below)
+ */
+// let isAuth = (authFactory) => new Promise((resolve, reject) => {
+//   if (authFactory.isAuthenticated()) {
+//     console.log("User is authenticated, resolve route promise");
+//     resolve();
+//   } else {
+//     console.log("User is not authenticated, reject route promise");
+//     reject();
+//   }
+// });
+
+tC.config(["$routeProvider", 
+	function ($routeProvider) {
+		$routeProvider
+		  // .when("/", {
+		  //   templateUrl: "partials/main.html",
+		  //   controller: "mainCtrl"
+		  // })
+		  .when("/", {
+		    templateUrl: "partials/login.html",
+		    controller: "loginCtrl"
+		  })
+		  // .when('/selector', {
+		  //   templateUrl: 'partials/login.html',
+		  //   controller: ""
+		  // })
+		  .otherwise({
+		    redirectTo: "/login"
+		  });
 
 }]);
+// tC.run([
+//   "$location",
+
+//   ($location) => {
+//     let myTCAppRef = new Firebase("https://resplendent-inferno-6925.firebaseio.com/");
+
+//     myTCAppRef.onAuth(authData => {
+//       if (!authData) {
+//         $location.path("/login");
+  //     }
+  //   });
+  // }
+// ]);
